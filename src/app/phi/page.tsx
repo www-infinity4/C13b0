47ebd1b9a35101f10e5f7e1b1cf8328b689cafb3
www-infinity4 <ephraim.nwo@gmail.com>
2026-09-08 01:58:54 -0500

    Restore Phi as the dedicated research page

--- src/app/phi/page.tsx
+++ src/app/phi/page.tsx
@@ -1,12 +1,1 @@
-"use client";
-
-import { useEffect } from "react";
-import { appPath } from "@/lib/base-path";
-
-export default function PhiRedirect() {
-  useEffect(() => {
-    location.replace(`${appPath("")}${location.search}`);
-  }, []);
-
-  return <main className="phi-build-loading">Opening Infinity Phi…</main>;
-}
+export { default } from "@/components/PhiPage";
