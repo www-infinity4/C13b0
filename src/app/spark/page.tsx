f2b3de7dc3e779808b3d69e55caeaf0b9dcdaea2
www-infinity4 <ephraim.nwo@gmail.com>
2026-09-08 01:38:22 -0500

    Collapse duplicate Spark index into the canonical Infinity index

--- src/app/spark/page.tsx
+++ src/app/spark/page.tsx
@@ -1,1 +1,12 @@
-export { default } from "../page";
+"use client";
+
+import { useEffect } from "react";
+import { appPath } from "@/lib/base-path";
+
+export default function SparkRedirect() {
+  useEffect(() => {
+    location.replace(`${appPath("")}${location.search}`);
+  }, []);
+
+  return <main className="phi-build-loading">Opening Infinity Phi…</main>;
+}
