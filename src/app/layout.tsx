dcab482a7f4539b953ed906ecc30adff8ee1757d
www-infinity4 <ephraim.nwo@gmail.com>
2026-09-09 00:51:20 -0500

    Load living Phi research styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import Script from "next/script";
 import "./globals.css";
+import "./phi-research.css";
 import SiteChrome from "@/components/SiteChrome";
 import AppRuntime from "@/components/AppRuntime";
 
